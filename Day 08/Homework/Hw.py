1f602ac567cf2db2c7f7d5347dca66452a39233c
DemetreBakuridze <demebakuridze6@gmail.com>
2025-10-18 09:40:59 -0700

    commit

---
+++ Day 08/Homework/Hw.py
@@ -0,0 +1,19 @@
+first_book_price = 50
+second_book_price = 60
+third_book_price = 40
+forth_book_price = 70
+fifth_book_price = 80
+
+discount_amount = 20
+
+the_new_price_of_first_book = first_book_price - discount_amount
+the_new_price_of_second_book = second_book_price - discount_amount
+the_new_price_of_third_book = third_book_price - discount_amount
+the_new_price_of_forth_book = forth_book_price - discount_amount
+the_new_price_of_fifth_book = fifth_book_price - discount_amount
+
+print(the_new_price_of_first_book)
+print(the_new_price_of_second_book)
+print(the_new_price_of_third_book)
+print(the_new_price_of_forth_book)
+print(the_new_price_of_fifth_book)
---
+++ Day 08/Homework/Hw2.py
@@ -0,0 +1,14 @@
+name = input("enter your name:")
+print("Hello,", name + "!")
+surname = input("enter your surname:")
+print("nice to meet you Demetre",surname + "!")
+age = input("enter your age:")
+print("Well,", name, "you are", age, "years old!")
+location = input("enter your location:")
+print("please")
+occupation = input("enter your occupation:")
+print("please")
+hobby = input("enter your hobby:")
+print(name, surname, "is", age, "years old,", location + "," "works as an", occupation + ",", "loves ", hobby + ".")
+
+
